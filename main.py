aad8b12851d822ef42ac8f4957bc90a2cf2d56a2
ben-bougher <ben.bougher@gmail.com>
2014-10-25 11:23:34 -0600

    hello world

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from jinja2 import Environment, FileSystemLoader
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+
+        # Load the main page welcome page
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class UploadModel(webapp2.RequestHandler):
+    pass
+    
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
