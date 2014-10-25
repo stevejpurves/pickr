05773ddbafc1797546c6b3eb52270f7fa33172f1
Jacob Foshee <jacobf@gmail.com>
2014-10-25 14:14:13 -0600

    Put image into a canvas

--- static/js/pickpoint.js
+++ static/js/pickpoint.js
@@ -1,9 +1,21 @@
 $(function() {
+    var seismicCanvas = $('#seismic-canvas');
+    console.log(seismicCanvas);
+    // Load image into canvas
+    var context = seismicCanvas[0].getContext('2d');
+    console.log(context);
+    var base_image = new Image();
+    base_image.src = '/static/data/Alaska.png';
+    base_image.onload = function(){
+        context.drawImage(base_image, 0, 0);
+    };
+
   // Handler for .ready() called.
-  $('#seismic-image').click(function(e) {
+  seismicCanvas.click(function(e) {
     var imageX = e.pageX - this.offsetLeft;
     var imageY = e.pageY - this.offsetTop;
-    $.post('/update_pick', { x: imageX, y: imageY });
-    //alert( "Handler for .click() called." + mouseX + "," + mouseY );
+    $.post('/update_pick', { x: imageX, y: imageY }, 
+        function(response){alert(response);});
+//    alert( "Handler for .click() called." + imageX + "," + imageY );
     });
 });
