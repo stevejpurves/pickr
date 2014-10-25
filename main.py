b1bca4808692e122547f6ea157b8c6f74e6c1990
ben-bougher <ben.bougher@gmail.com>
2014-10-25 12:20:00 -0600

    fixed block errors

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import webapp2
+from jinja2 import Environment, FileSystemLoader
+from os.path import dirname, join
+import os
 
 from jinja2 import Environment, FileSystemLoader
 
+# Jinja2 environment to load templates
+env = Environment(loader=FileSystemLoader(join(dirname(__file__),
+                                               'templates')))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
         # Load the main page welcome page
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        template = env.get_template('main.html')
+        self.response.write(template.render())
+
+
+class AboutHandler(webapp2.RequestHandler):
+
+    def get(self):
+
+        # Load the main page welcome page
+        template = env.get_template('about.html')
+        self.response.write(template.render())
 
         
 class UploadModel(webapp2.RequestHandler):
@@ -16,5 +31,6 @@ class UploadModel(webapp2.RequestHandler):
     
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/upload', UploadModel)
+    ('/upload', UploadModel),
+    ('/about', AboutHandler)
 ], debug=True)
