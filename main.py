d44f0b36d6689978ca978b14de032aaa9f47b752
ben-bougher <ben.bougher@gmail.com>
2014-10-25 13:14:09 -0600

    commented out broken code, updated params in request

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 from jinja2 import Environment, FileSystemLoader
 
+#from lib_db import SeismicObject
 # Jinja2 environment to load templates
 env = Environment(loader=FileSystemLoader(join(dirname(__file__),
                                                'templates')))
@@ -26,46 +27,45 @@ class AboutHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 
 
-class UploadHandler(blobstore_handlers.BlobstoreUploadHandler,
-                    webapp2.RequestHandler):
+## class UploadHandler(blobstore_handlers.BlobstoreUploadHandler,
+##                     webapp2.RequestHandler):
 
-    def post(self):
+##     def post(self):
 
-        upload_file = self.get_uploads()
-        blob_info = upload_files[0]
+##         upload_file = self.get_uploads()
+##         blob_info = upload_files[0]
 
-        # Read the image file
-        reader = blobstore.BlobReader(blob_info.key())
+##         # Read the image file
+##         reader = blobstore.BlobReader(blob_info.key())
 
-        im = Image.open(reader, 'r')
-        im = im.convert('RGB').resize((350,350))
+##         im = Image.open(reader, 'r')
+##         im = im.convert('RGB').resize((350,350))
 
-        output = StringIO.StringIO()
-        im.save(output, format='PNG')
+##         output = StringIO.StringIO()
+##         im.save(output, format='PNG')
 
-        bucket = '/pickr_bucket/'
-        output_filename = (bucket +'/2' + str(time.time()))
+##         bucket = '/pickr_bucket/'
+##         output_filename = (bucket +'/2' + str(time.time()))
 
+##         gcsfile = gcs.open(output_filename, 'w')
+##         gcsfile.write(output.getvalue())
 
-        gcsfile = gcs.open(output_filename, 'w')
-        gcsfile.write(output.getvalue())
+##         output.close()
+##         gcsfile.close()
 
-        output.close()
-        gcsfile.close()
+##         # Make a blob reference
+##         bs_file = '/gs' + output_filename
+##         output_blob_key = blobstore.create_gs_key(bs_file)
 
-        # Make a blob reference
-        bs_file = '/gs' + output_filename
-        output_blob_key = blobstore.create_gs_key(bs_file)
+##         name = self.request.get("name")
+##         description = self.request.get("description")
 
-        name = self.request.get("name")
-        description = self.request.get("description")
+##         new_db = SeismicObject(name=name, description=description,
+##                                image=output_blob_key)
 
-        new_db = SeismicObject(name=name, description=description,
-                               image=output_blob_key)
+##         new_db.put()
 
-        new_db.put()
-
-        self.redirect('/')
+##         self.redirect('/')
         
         
 
@@ -73,24 +73,32 @@ class UpdatePick(webapp2.RequestHandler):
 
     def post(self):
 
-        point = self.request.get("point")
+        point = (self.request.get("x"),
+                 self.request.get("y"))
+
+        print "POINT",  point
+        #d = SeismicObject.all().get()
+
+        #picks = json.loads(d.picks)
+        #picks.append(point)
+        #d.picks = json.dumps(picks).encode()
         self.response.write("Ok")
         
-class AddImageHandler(webapp2.RequestHandler):
+## class AddImageHandler(webapp2.RequestHandler):
 
-    def get(self):
+##     def get(self):
 
-        upload_url = blobstore.create_upload_url('/upload')
+##         upload_url = blobstore.create_upload_url('/upload')
 
-        template = env.get_template("new_image.html")
+##         template = env.get_template("new_image.html")
 
-        html = template.render(upload_url=upload_url))
-        self.response.write(html)
+##         html = template.render(upload_url=upload_url))
+##         self.response.write(html)
     
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/upload', UploadModel),
+    #('/upload', UploadModel),
     ('/about', AboutHandler),
-    ('/new_image', AddImageHandler),
+    #('/new_image', AddImageHandler),
     ('/update_pick', UpdatePick)],
     debug=True)
