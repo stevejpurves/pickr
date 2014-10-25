85711e20f369b50c14752ce0ba86c76567da67c8
Jacob Foshee <jacobf@gmail.com>
2014-10-25 14:22:57 -0600

    Drawing clicked points (that were successfully posted to server)

--- static/js/pickpoint.js
+++ static/js/pickpoint.js
@@ -1,21 +1,31 @@
 $(function() {
     var seismicCanvas = $('#seismic-canvas');
-    console.log(seismicCanvas);
-    // Load image into canvas
     var context = seismicCanvas[0].getContext('2d');
-    console.log(context);
+
+    // Load image into canvas
     var base_image = new Image();
     base_image.src = '/static/data/Alaska.png';
     base_image.onload = function(){
         context.drawImage(base_image, 0, 0);
     };
+    
+    var drawCircle = function(x, y)
+    {
+        context.beginPath();
+        var radius = 10;
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'green';
+        context.fill();
+        context.lineWidth = 2;
+        context.strokeStyle = '#003300';
+        context.stroke();
+    }
 
-  // Handler for .ready() called.
-  seismicCanvas.click(function(e) {
-    var imageX = e.pageX - this.offsetLeft;
-    var imageY = e.pageY - this.offsetTop;
-    $.post('/update_pick', { x: imageX, y: imageY }, 
-        function(response){alert(response);});
-//    alert( "Handler for .click() called." + imageX + "," + imageY );
+    seismicCanvas.click(function(e) {
+        var imageX = e.pageX - this.offsetLeft;
+        var imageY = e.pageY - this.offsetTop;
+        $.post('/update_pick', { x: imageX, y: imageY }, 
+            function(response){
+                drawCircle(imageX, imageY);});
     });
 });
