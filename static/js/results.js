6129e0afcb3c4e3ccd55bb2d1f2eeadcc135ecb4
Jacob Foshee <jacobf@gmail.com>
2014-10-26 13:27:03 -0600

    Can page through all interpretations (previous and next buttons)

--- static/js/results.js
+++ static/js/results.js
@@ -1,3 +1,32 @@
 $(function() {
+    var interpretationCount = count;
+    var current = 0;
+    pickDrawing.setup('seismic-div');
     
+    var loadPicks = function()
+    {
+        pickDrawing.load({
+            pick_index: current
+        });
+    }
+    loadPicks();
+
+
+    $('#previous-button').click(function(){
+        if (current <= 0)
+            return;
+        --current;
+        loadPicks();
+    });
+
+    $('#next-button').click(function(){
+        if (current >= interpretationCount - 1)
+            return;
+        ++current;
+        loadPicks();
+    });
+    
+    // get /update_pick?pick_index=1
+    // get /vote?index=1
+    // post /vote?index=1&vote=1 
 });
