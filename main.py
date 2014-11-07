7f17e393bdd0a364f72ee07a1e46206fecf3422c
kwinkunks <matt@agilegeoscience.com>
2014-11-07 11:16:04 -0400

    refectored result code

--- main.py
+++ main.py
@@ -7,31 +7,27 @@ import base64
 import hashlib
 import StringIO
 import time
+import numpy as np
 
 from google.appengine.api import users
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import blobstore
 from google.appengine.api import images
 
+# For image manipulation
 from PIL import Image
-
-
+from pickthis import get_result_image
 
 # For image serving
 import cloudstorage as gcs
 
-import numpy as np
+# Pick This objects
+from lib_db import ImageObject, SeismicParent, SeismicPicks
+
 if not os.environ.get('SERVER_SOFTWARE','').startswith('Development'):
-    
-    import matplotlib.pyplot as plt
-    import matplotlib.cm as cm
-
-
     local = False
 else:
     local = True
-
-from lib_db import ImageObject, SeismicParent, SeismicPicks
 
 # Jinja2 environment to load templates.
 env = Environment(loader=FileSystemLoader(join(dirname(__file__),
@@ -42,6 +38,29 @@ db_parent = SeismicParent.all().get()
 if not db_parent:
     db_parent = SeismicParent()
     db_parent.put()
+
+
+# Make a basic PageRequest class to handle the params we always need...
+class PickThisPageRequest(webapp2.RequestHandler):
+    
+    def get_base_params(self, **kwargs):
+
+        user = users.get_current_user()
+        
+        if user:
+            logout_url = users.create_logout_url('/')
+            login_url = None
+            email_hash = hashlib.md5(user.email()).hexdigest()
+        else:
+            logout_url = None
+            login_url = users.create_login_url('/')
+            email_hash = ''
+
+        params = dict(logout_url=logout_url,
+                      login_url=login_url,
+                      email_hash=email_hash)
+
+        return params
 
 
 class CommentHandler(webapp2.RequestHandler):
@@ -113,7 +132,6 @@ class MainPage(webapp2.RequestHandler):
         if not user:
             login_url = users.create_login_url('/')
             template = env.get_template("main.html")
-
             html = template.render(login_url=login_url)
             self.response.out.write(html)
 
@@ -126,174 +144,47 @@ class MainPage(webapp2.RequestHandler):
             
 
 class ResultsHandler(webapp2.RequestHandler):
-
     def get(self):
-
-        # connect the dots using one dimensional linear interpretation: np.interp()
-        def regularize(xarr, yarr, px, py):
-            # connect the dots of the horizon spanning the image
-            # px : is the number of horizontal pixels
-            # py : is the number of horizontal pixels
-            horx = np.arange(px)
-            hory = np.interp(horx, xarr, yarr)
-            return horx, hory
-        
-        # append all horizons into one big file
-        all_picks_x = np.array([])
-        all_picks_y = np.array([])
 
         image_key = self.request.get("image_key")
         img_obj = ImageObject.get_by_id(int(image_key),
                                         parent=db_parent)
         image_url = images.get_serving_url(img_obj.image)
+
+        image, count = get_result_image(img_obj)
         
-        
-        data = SeismicPicks.all().ancestor(img_obj).fetch(1000)
-        
-        count = len(data)
-
-        if not local:
-            
-            fig = plt.figure(figsize=(15,8))
-            ax = fig.add_axes([0,0,1,1])
-        
-            # Load the image to a variable
-            im = Image.open('brazil_ang_unc.png')
-            px, py = im.size
-        
-            # plot the seismic image first
-            # im = plt.imshow(im)
-            # Make a modified version of rainbow colormap with some transparency
-            # in the bottom of the colormap.
-            hot = cm.hot
-            hot.set_under(alpha = 0.0)  #anything that has value less than 0.5 goes transparent
-            
-            for user in data:
-                try:
-                    picks = np.array(json.loads(user.picks))
-                    hx, hy = regularize(picks[:,0], picks[:,1], px, py)
-                    all_picks_x = np.concatenate((all_picks_x,hx))
-                    all_picks_y = np.concatenate((all_picks_y,hy))
-                    ax.plot(picks[:,0], picks[:,1], 'g-', alpha=0.5, lw=2)
-
-                    m = 1
-                    # do 2d histogram to display heatmap
-                    binsizex = m
-                    binsizey = m
-                    heatmap, yedges, xedges = np.histogram2d(all_picks_y, all_picks_x,
-                                                             bins=(720/binsizex,1080/binsizey),
-                                                             range=np.array([[0, 720], [0,1080]]))
-                    extent = [0, 1080,
-                              720, 0 ]
-
-                    # do dilation of picks in heatmap
-                    heatmap_dil = heatmap#grey_dilation(heatmap,size=(5,5))
-                
-                    #fig = plt.figure(figsize=(15,8))
-                    #ax = fig.add_axes([0, 0, 1, 1])
-                    #heatim = ax.imshow(heatmap,
-                    #                   cmap=cm.hot, extent=extent, alpha=0.75)
-                    #heatim.set_clim(0.5, np.amax(heatmap))
-                    ax.set_ylim((720,0))
-                    ax.set_xlim((0,1080))
-                    #ax.invert_yaxis()
-                    ax.set_xticks([])
-                    ax.set_yticks([])
-                    ax.set_frame_on(False)
-                except:
-                    pass
-                
-            output = StringIO.StringIO()
-            plt.savefig(output)
-            image = base64.b64encode(output.getvalue())
-
-            user = users.get_current_user()
-
-            # User should exist, so this should fail otherwise.
-            logout_url = users.create_logout_url('/')
-            login_url = None
-            email_hash = hashlib.md5(user.email()).hexdigest()
-
-            template = env.get_template("results.html")
-            html = template.render(count=count,
-                                   logout_url=logout_url,
-                                   email_hash=email_hash,
-                                   image=image,
-                                   image_url=image_url,
-                                   image_key=image_key)
-
-            self.response.write(html)
-            
-
-        else:
-
-            with open("alaska.b64", "r") as f:
-                image = f.read()
-                
-            user = users.get_current_user()
-
-            # User should exist, so this should fail otherwise.
-            logout_url = users.create_logout_url('/')
-            login_url = None
-            email_hash = hashlib.md5(user.email()).hexdigest()
-            
-            template = env.get_template("results.html")
-            html = template.render(count=count,
-                                   logout_url=logout_url,
-                                   email_hash=email_hash,
-                                   image=image,
-                                   image_url=image_url,
-                                   image_key=image_key)
-                
-            self.response.write(html)
-
-        # Make composite image
-
-
-class AboutHandler(webapp2.RequestHandler):
-
-    def get(self):
 
         user = users.get_current_user()
-        
-        if user:
-            logout_url = users.create_logout_url('/')
-            login_url = None
-            email_hash = hashlib.md5(user.email()).hexdigest()
-        else:
-            logout_url = None
-            login_url = users.create_login_url('/')
-            email_hash = ''
 
-        # Write the page.
-        template = env.get_template('about.html')
-        html = template.render(logout_url=logout_url,
-                               login_url=login_url,
-                               email_hash=email_hash)
+        # User should exist, so this should fail otherwise.
+        logout_url = users.create_logout_url('/')
+        login_url = None
+        email_hash = hashlib.md5(user.email()).hexdigest()
+
+        template = env.get_template("results.html")
+        html = template.render(count=count,
+                                logout_url=logout_url,
+                                email_hash=email_hash,
+                                image=image,
+                                image_url=image_url,
+                                image_key=image_key)
+
         self.response.write(html)
 
 
-class TermsHandler(webapp2.RequestHandler):
-
+class AboutHandler(PickThisPageRequest):
     def get(self):
+        template_params = self.get_base_params()
+        template = env.get_template('about.html')
+        html = template.render(template_params)
+        self.response.write(html)
 
-        user = users.get_current_user()
-        
-        if user:
-            logout_url = users.create_logout_url('/')
-            login_url = None
-            email_hash = hashlib.md5(user.email()).hexdigest()
-        else:
-            logout_url = None
-            login_url = users.create_login_url('/')
-            email_hash = ''
 
-        # Write the page.
+class TermsHandler(PickThisPageRequest):
+    def get(self):
+        template_params = self.get_base_params()
         template = env.get_template('terms.html')
-        html = template.render(logout_url=logout_url,
-                               login_url=login_url,
-                               email_hash=email_hash)
-
+        html = template.render(template_params)
         self.response.write(html)
 
 
@@ -514,21 +405,9 @@ class LibraryHandler(blobstore_handlers.BlobstoreUploadHandler,
                       str(new_db.key().id())) 
         
         
-class AddImageHandler(webapp2.RequestHandler):
+class AddImageHandler(PickThisPageRequest):
 
     def get(self):
-
-        user = users.get_current_user()
-        
-        if user:
-            logout_url = users.create_logout_url('/')
-            login_url = None
-            email_hash = hashlib.md5(user.email()).hexdigest()
-        else:
-            # Should never be here
-            logout_url = None
-            login_url = users.create_login_url('/')
-            email_hash = ''
 
         image_key = self.request.get("image_key")
 
@@ -538,14 +417,12 @@ class AddImageHandler(webapp2.RequestHandler):
         image_url = images.get_serving_url(img_obj.image)
 
         
+        template_params = self.get_base_params()
+        template_params.update(image_url=image_url,
+                               image_key=image_key)
+
         template = env.get_template("add_image.html")
-
-        html = template.render(image_url=image_url,
-                               image_key=image_key,
-                               logout_url=logout_url,
-                               login_url=login_url,
-                               email_hash=email_hash)
-
+        html = template.render(template_params)
         self.response.write(html)
 
     def post(self):
@@ -571,6 +448,7 @@ class AddImageHandler(webapp2.RequestHandler):
         img_obj.put()
 
         self.redirect('/')
+
 
 # This is the app.  
 app = webapp2.WSGIApplication([
---
+++ pickthis.py
@@ -0,0 +1,77 @@
+
+import numpy as np
+import StringIO, json, base64
+
+from lib_db import SeismicPicks
+
+from google.appengine.api import images
+from google.appengine.ext import blobstore
+
+# For image manipulation
+from PIL import Image
+from mmorph import dilate
+
+# Connect the dots using one dimensional linear interpretation: np.interp()
+def regularize(xarr, yarr):
+    # Connect the dots of the horizon spanning the image.
+    horx = np.arange(np.amin(xarr),np.amax(xarr)+1)
+    hory = np.interp(horx, xarr, yarr)
+    return horx, hory
+    
+def normalize(arr):
+    maxval = np.amax(arr)
+    normalized = 255.0*(arr)/maxval
+    return normalized
+
+def get_result_image(img_obj):
+
+        data = SeismicPicks.all().ancestor(img_obj).fetch(1000)
+        
+        count = len(data)
+        
+        # Append all horizons into one big file
+        all_picks_x = np.array([])
+        all_picks_y = np.array([])   
+        
+        # Get the dimensions.
+        reader = blobstore.BlobReader(img_obj.image)
+        im = Image.open(reader, 'r')
+        px, py = im.size
+
+        # Get all the interpretations.
+        for user in data:
+            picks = np.array(json.loads(user.picks))
+            hx, hy = regularize(picks[:,0], picks[:,1])
+            all_picks_x = np.concatenate((all_picks_x, hx))
+            all_picks_y = np.concatenate((all_picks_y, hy))
+                                     
+        # Make a 2d histogram (an image) of the heatmap.
+        m = 1
+        r = np.array([[0, py], [0, px]])
+        heatmap, yedges, xedges = np.histogram2d(all_picks_y,
+                                                 all_picks_x,
+                                                 bins=(py/m,px/m),
+                                                 range=r) 
+
+        # Do dilation of the histogram 'image' in heatmap.
+        n = 3   # The dilation structuring element (n x n).
+        heatmap_morph = dilate( heatmap.astype(int), B = np.ones((n,n)).astype(int) )
+
+        # Normalize the heatmap from 0-255 for making an image.
+        heatmap_norm = normalize(heatmap_morph)
+        
+        # Make the RGB channels.
+        r, g, b = heatmap_norm, heatmap_norm, heatmap_norm
+        a = 200*np.ones(heatmap_norm.shape)
+        
+        #a[r == 0] = 255
+    
+        x = np.dstack([r, g, b, a])
+
+        
+
+        im_out = Image.fromarray(x.astype('uint8'), 'RGBA')
+        output = StringIO.StringIO()
+        im_out.save(output, 'png')
+        
+        return base64.b64encode(output.getvalue()), count
