031082c50e143acec19768a1809283c4b58d5e06
Steve Purves <stevejpurves@gmail.com>
2015-02-19 16:15:21 +0000

    restored undo feature

--- static/js/pickpoint.js
+++ static/js/pickpoint.js
@@ -9,30 +9,49 @@ var Point = function(X, Y) {
     return this;
 };
 
+var UndoStack = function()
+{
+    var the_stack = [];
+    this.action = { ADD: 'add', MOVE: 'move' };
+    this.push = function(a, idx, old_p) {
+        the_stack.push( {action: a, idx: idx, old_point: old_p} )
+    }
+    this.pop = function()
+    {
+        var popped = the_stack[the_stack.length-1];
+        the_stack.pop();
+        return popped;
+    }
+    return this;
+}
+
 var PointList = function()
 {
     var tol = 0;
     var points = [];
-    var undo_stack = [];
+    var undo_stack = new UndoStack();
     this.get_points = function(x) { return points; };
     this.add = function(x) {
                 points.push(x);
-                undo_stack.push(points.length-1);
+                undo_stack.push(undo_stack.action.ADD, points.length-1);
             };
     this.remove_last = function() {
-                points.splice(undo_stack[undo_stack.length-1], 1);
-                undo_stack.pop();
+                var last = undo_stack.pop();
+                if (last.action === undo_stack.action.MOVE)
+                    points[last.idx] = last.old_point;
+                else
+                    points.splice(last.idx, 1);
             };
     this.clear = function() { points = []; };
     this.setTolerance = function(t) { tol = t; };
     this.insertAt = function(p, at) {
         points.splice(at+1, 0, p);
-        undo_stack.push(at+1);
+        undo_stack.push(undo_stack.action.ADD, at+1);
     };
-
     this.replace = function(p_old, p_new) {
         for (var i = 0; i < points.length; i++)
             if (points[i].equals(p_old, tol)) {
+                undo_stack.push(undo_stack.action.MOVE, i, points[i])
                 points[i] = p_new;
                 return true;
             }
