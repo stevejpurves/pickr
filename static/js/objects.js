90b8d1bc94e7d1ba2e2d49c984b207259f2329f8
Steve Purves <stevejpurves@gmail.com>
2015-02-25 21:51:29 +0000

    simplifed history and tied into undo

--- static/js/objects.js
+++ static/js/objects.js
@@ -12,26 +12,34 @@ var Point = function(X, Y) {
 var PickHistory = function() {
     var history = []
 
+    var last = function() {
+        if (history.length === 0) return
+        console.log(history)
+        return history[history.length-1]
+    }
+
     this.clear = function() {
-        history.push({action:'clear'})
+        history = []
     }
-    this.add = function(point) {
-        history.push({action: 'add', point: point})
+
+    this.log_add = function(idx, point) {
+        history.push({action: 'add', idx: idx, point: point})
+        return last()
     }
-    this.insert = function(idx, point) {
+    this.log_insert = function(idx, point) {
         history.push({action:'insert', idx: idx, point: point})
+        return last()
     }
-    this.move = function(idx, to, from) {
+    this.log_move = function(idx, to, from) {
         history.push({action:'move', idx: idx, to: to, from: from})
+        return last()
     }
-    this.remove = function(idx) {
-        history.push({action:'remove', idx: idx})
+    this.log_remove = function(idx, point) {
+        history.push({action:'remove', idx: idx, point: point})
+        return last()
     }
     this.json = function() {
         return JSON.stringify(history)
-    }
-    this.last_undoable = function() {
-        if (history.length === 0) return
     }
     return this;
 }
@@ -39,9 +47,9 @@ var PickHistory = function() {
 var UndoStack = function()
 {
     var the_stack = [];
-    this.action = { ADD: 'add', MOVE: 'move' };
-    this.push = function(a, idx, old_p) {
-        the_stack.push( {action: a, idx: idx, old_point: old_p} )
+
+    this.push = function(historyItem) {
+        the_stack.push(historyItem)
     }
     this.pop = function()
     {
@@ -56,34 +64,51 @@ var PointList = function()
 {
     var tol = 0;
     var points = [];
+    var history = new PickHistory();
     var undo_stack = new UndoStack();
-    this.get_points = function(x) { return points; };
-    this.add = function(x) {
-                points.push(x);
-                undo_stack.push(undo_stack.action.ADD, points.length-1);
-            };
-    this.remove_last = function() {
-                var last = undo_stack.pop();
-                if (last.action === undo_stack.action.MOVE)
-                    points[last.idx] = last.old_point;
-                else
-                    points.splice(last.idx, 1);
-            };
-    this.clear = function() { points = []; };
-    this.setTolerance = function(t) { tol = t; };
+    
+    this.get_points = function() {
+        return points;
+    };
+
+    this.add = function(p) {
+        points.push(p);
+        undo_stack.push(history.log_add(points.length-1, p));
+    };
+
     this.insertAt = function(p, at) {
         points.splice(at+1, 0, p);
-        undo_stack.push(undo_stack.action.ADD, at+1);
+        undo_stack.push(history.log_add(at+1, p));
     };
+
     this.replace = function(p_old, p_new) {
         for (var i = 0; i < points.length; i++)
             if (points[i].equals(p_old, tol)) {
-                undo_stack.push(undo_stack.action.MOVE, i, points[i])
+                undo_stack.push(history.log_move(i, p_new, points[i]))
                 points[i] = p_new;
                 return true;
             }
         return false;
     };
+
+    this.remove_last = function() {
+        var last = undo_stack.pop();
+        if (last.action === 'move') {
+            this.replace(points[last.idx], last.from)
+            undo_stack.pop()
+        }
+        else {
+            history.log_remove(last.idx, points[last.idx])
+            points.splice(last.idx, 1);
+        }
+    };
+
+    this.clear = function() { 
+        points = []
+        history.clear()
+    };
+
+    this.setTolerance = function(t) { tol = t; };
 
     return this;
 };
--- test/pickhistory.spec.js
+++ test/pickhistory.spec.js
@@ -1,32 +1,7 @@
 var expect = chai.expect
 
 
-function PickHistory() {
-	var history = []
 
-	this.clear = function() {
-		history.push({action:'clear'})
-	}
-	this.add = function(point) {
-		history.push({action: 'add', point: point})
-	}
-	this.insert = function(idx, point) {
-		history.push({action:'insert', idx: idx, point: point})
-	}
-	this.move = function(idx, to, from) {
-		history.push({action:'move', idx: idx, to: to, from: from})
-	}
-	this.remove = function(idx) {
-		history.push({action:'remove', idx: idx})
-	}
-	this.json = function() {
-		return JSON.stringify(history)
-	}
-	this.last_undoable = function() {
-		if (history.length === 0) return
-	}
-	return this;
-}
 
 describe("Frontend", function() {
 	
@@ -42,51 +17,69 @@ describe("Frontend", function() {
 			history = new PickHistory();
 		})
 
-		it("reports in json", function() {
-			expect(history.json()).to.be.a('string')
-			expect(function() {report()}).to.not.throw(Error)
-		})
-
-		it("returns an empty array, when empty", function() {
+		it("can be cleared", function() {
+			history.log_add(1)
+			history.clear()
 			expect(report()).to.be.empty
-		})
+		});
 
 		describe("LOGS pick actions", function() {
 			it("adding a pick", function() {
-				var p = new Point(2,3)
-				history.add(p)
-				expect(report()[0].action).to.equal('add')
-				expect(report()[0].point.x).to.equal(2)
-				expect(report()[0].point.y).to.equal(3)
-			})
-
-			it("inserting a pick", function() {
-				history.insert(3, new Point(3,4))
-				expect(report()[0].action).to.equal('insert')
-				expect(report()[0].idx).to.equal(3)
-				expect(report()[0].point.x).to.equal(3)
-				expect(report()[0].point.y).to.equal(4)
+				var item = history.log_add(4, new Point(2,3))
+				expect(item.idx).to.equal(4)
+				expect(item.action).to.equal('add')
+				expect(item.point.x).to.equal(2)
+				expect(item.point.y).to.equal(3)
 			})
 			
 			it("moving a pick", function() {
-				history.move(4, new Point(5,6), new Point(1,2))
-				expect(report()[0].action).to.equal('move')
-				expect(report()[0].idx).to.equal(4)
-				expect(report()[0].to.x).to.equal(5)
-				expect(report()[0].to.y).to.equal(6)				
-				expect(report()[0].from.x).to.equal(1)
-				expect(report()[0].from.y).to.equal(2)
+				var item = history.log_move(4, new Point(5,6), new Point(1,2))
+				expect(item.action).to.equal('move')
+				expect(item.idx).to.equal(4)
+				expect(item.to.x).to.equal(5)
+				expect(item.to.y).to.equal(6)				
+				expect(item.from.x).to.equal(1)
+				expect(item.from.y).to.equal(2)
 			})
 
 			it("removing a pick", function() {
-				history.remove(7)
-				expect(report()[0].action).to.equal('remove')
-				expect(report()[0].idx).to.equal(7)
+				var item = history.log_remove(7, new Point(7,8))
+				expect(item.action).to.equal('remove')
+				expect(item.idx).to.equal(7)
+				expect(item.point.x).to.equal(7)
+				expect(item.point.y).to.equal(8)				
+			})
+		})
+
+		describe("reports", function() {
+			it("in json", function() {
+				expect(history.json()).to.be.a('string')
+				expect(function() {report()}).to.not.throw(Error)
 			})
 
-			it("clearing all picks", function() {
-				history.clear()
-				expect(report()[0].action).to.equal('clear')
+			it("are empty when no history", function() {
+				expect(report()).to.be.empty
+			})
+
+			it("have all history items listed in order", function() {
+				history.log_add(4, new Point(2,3))
+				history.log_move(4, new Point(5,6), new Point(1,2))
+				history.log_remove(7, new Point(7,8))
+
+				expect(report()[0].action).to.equal('add')
+				expect(report()[0].idx).to.equal(4)
+				expect(report()[0].point.x).to.equal(2)
+				expect(report()[0].point.y).to.equal(3)
+				expect(report()[1].action).to.equal('move')
+				expect(report()[1].idx).to.equal(4)
+				expect(report()[1].to.x).to.equal(5)
+				expect(report()[1].to.y).to.equal(6)				
+				expect(report()[1].from.x).to.equal(1)
+				expect(report()[1].from.y).to.equal(2)	
+				expect(report()[2].action).to.equal('remove')
+				expect(report()[2].idx).to.equal(7)
+				expect(report()[2].point.x).to.equal(7)
+				expect(report()[2].point.y).to.equal(8)			
 			})
 		})
 	})
