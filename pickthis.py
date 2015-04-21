e8afcec6010a3689c5c42a7efdaf911e3ed8a256
Steve Purves <stevejpurves@gmail.com>
2015-04-21 10:36:06 +0100

    refactoring of heatmap code and getting groups working

--- pickthis.py
+++ pickthis.py
@@ -2,7 +2,7 @@
 import numpy as np
 import StringIO, json, base64
 import time
-
+import itertools
 from lib_db import Picks, ImageObject, User, Vote, Heatmap
 
 from google.appengine.api import images
@@ -43,11 +43,75 @@ def normalize(a, newmax):
     """
     return (float(newmax) * a) / np.amax(a)
 
+def draw_pick_to_user_image(user_image, picks, img_obj):
+    w = img_obj.width
+    h = img_obj.height
+    pickstyle = img_obj.pickstyle    
+
+    if pickstyle == 'polygons':
+        picks = np.append(picks, picks[0]).reshape(picks.shape[0]+1, picks.shape[1])
+
+    # Deal with the points, and set the
+    # radius of the disk structuring element.
+    if pickstyle != 'points':
+        for i, pick in enumerate(picks[:-1]):
+
+            xpair = picks[i:i+2, 0]
+
+            if xpair[0] > xpair[1]:
+                xpair = xpair[xpair[:].argsort()]
+                xrev = True
+            else:
+                xrev = False
+
+            ypair = picks[i:i+2, 1]
+
+            if ypair[0] > ypair[1]:
+                ypair = ypair[ypair[:].argsort()]
+                yrev = True
+            else:
+                yrev = False
+
+            # Do the interpolation
+            x, y = interpolate(xpair, ypair)
+
+            if xrev:  # then need to unreverse...
+                x = x[::-1]
+            if yrev:  # then need to unreverse...
+                y = y[::-1]
+
+            # Build up the image, accounting for pixels at
+            # the edge, which have the wrong indices.
+            x[x >= w] = w - 1
+            y[y >= h] = h - 1
+            user_image[(y, x)] = 1.
+
+    else:
+        x, y = picks[:, 0], picks[:, 1]
+        user_image[(y, x)] = 1.
+
+def draw_all_picks_to_user_image(user_image, all_picks, img_obj):
+    if all_picks[0].size == 3:
+        # picks are tagged with their group
+        for group in itertools.groupby(all_picks, lambda x:x[2]):
+            picks = np.array([p for p in group[1]])
+            draw_pick_to_user_image(user_image, picks, img_obj)
+    else:
+        draw_pick_to_user_image(user_image, all_picks, img_obj)
+
+def calculate_disk_radius(img_obj):
+    w = img_obj.width
+    h = img_obj.height
+    avg = (w + h) / 2.
+    if (img_obj.pickstyle == 'points'):
+        n = np.ceil(avg / 150.).astype(int)
+    else:
+        n = np.ceil(avg / 300.).astype(int)
+    return n
 
 def generate_heatmap(img_obj, data, opacity_scalar):
     w = img_obj.width
     h = img_obj.height
-    avg = (w + h) / 2.
 
     # Make an 'empty' image for all the results.
     heatmap_image = np.zeros((h, w))
@@ -70,61 +134,15 @@ def generate_heatmap(img_obj, data, opacity_scalar):
         user_image = np.zeros((h, w))
 
         # Get the points.
-        picks = np.array(json.loads(user.picks))
+        all_picks = np.array(json.loads(user.picks))
         
-        if picks.size == 0:
+        if all_picks.size == 0:
             continue
 
-        if img_obj.pickstyle == 'polygons':
-            picks = np.append(picks, picks[0]).reshape(picks.shape[0]+1, picks.shape[1])
-
-        # Deal with the points, and set the
-        # radius of the disk structuring element.
-        if img_obj.pickstyle != 'points':
-            for i, pick in enumerate(picks[:-1]):
-
-                xpair = picks[i:i+2, 0]
-
-                if xpair[0] > xpair[1]:
-                    xpair = xpair[xpair[:].argsort()]
-                    xrev = True
-                else:
-                    xrev = False
-
-                ypair = picks[i:i+2, 1]
-
-                if ypair[0] > ypair[1]:
-                    ypair = ypair[ypair[:].argsort()]
-                    yrev = True
-                else:
-                    yrev = False
-
-                # Do the interpolation
-                x, y = interpolate(xpair, ypair)
-
-                if xrev:  # then need to unreverse...
-                    x = x[::-1]
-                if yrev:  # then need to unreverse...
-                    y = y[::-1]
-
-                # Build up the image, accounting for pixels at
-                # the edge, which have the wrong indices.
-                x[x >= w] = w - 1
-                y[y >= h] = h - 1
-                user_image[(y, x)] = 1.
-
-            n = np.ceil(avg / 300.).astype(int)
-
-        else:
-            x, y = picks[:, 0], picks[:, 1]
-            user_image[(y, x)] = 1.
-
-            # The radius of the disk structuring element
-            n = np.ceil(avg / 150.).astype(int)
-
+        draw_all_picks_to_user_image(user_image, all_picks, img_obj)
+        n = calculate_disk_radius(img_obj)
         # Dilate this image.
-        dilated_image = dilate(user_image.astype(int),
-                               B=sedisk(r=n))
+        dilated_image = dilate(user_image.astype(int), B=sedisk(r=n))
 
         heatmap_image += dilated_image
 
